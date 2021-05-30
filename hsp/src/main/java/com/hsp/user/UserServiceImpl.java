e31b65d29e3829622acf25d7e8cc14a9faa6b637
piltong11 <piltong10@gmail.com>
2021-05-30 21:22:28 +0900

    0530 zoom

---
+++ hsp/src/main/java/com/hsp/user/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.hsp.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean login(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User viewUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findID(String input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean findPW(String input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean certEmail(String input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
