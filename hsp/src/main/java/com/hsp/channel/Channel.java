9383d3dc01508ce84f7f9af533a81c5532cfd54c
piltong11 <piltong10@gmail.com>
2021-05-30 21:18:49 +0900

    0530 오후9시 줌 회의

--- hsp/src/main/java/com/hsp/channel/Channel.java
+++ hsp/src/main/java/com/hsp/channel/Channel.java
@@ -1,5 +1,92 @@
 package com.hsp.channel;
 
-public class Channel {
+import java.io.Serializable;
 
+public class Channel implements Serializable {
+	private String channel_id;
+	private String user_id;
+	private String channel_name;
+	private String category;
+	private String introduction;
+	private int delivery_fee;
+	private int business_no;
+	
+	public Channel() {
+		
+	}
+	
+	public Channel(String channel_id, String user_id, String channel_name, String category, String introduction,
+			int delivery_fee, int business_no) {
+		this.channel_id = channel_id;
+		this.user_id = user_id;
+		this.channel_name = channel_name;
+		this.category = category;
+		this.introduction = introduction;
+		this.delivery_fee = delivery_fee;
+		this.business_no = business_no;
+	}
+
+	public String getChannel_id() {
+		return channel_id;
+	}
+
+	public void setChannel_id(String channel_id) {
+		this.channel_id = channel_id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getChannel_name() {
+		return channel_name;
+	}
+
+	public void setChannel_name(String channel_name) {
+		this.channel_name = channel_name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	public int getDelivery_fee() {
+		return delivery_fee;
+	}
+
+	public void setDelivery_fee(int delivery_fee) {
+		this.delivery_fee = delivery_fee;
+	}
+
+	public int getBusiness_no() {
+		return business_no;
+	}
+
+	public void setBusiness_no(int business_no) {
+		this.business_no = business_no;
+	}
+
+	@Override
+	public String toString() {
+		return "Channel [channel_id=" + channel_id + ", user_id=" + user_id + ", channel_name=" + channel_name
+				+ ", category=" + category + ", introduction=" + introduction + ", delivery_fee=" + delivery_fee
+				+ ", business_no=" + business_no + "]";
+	}
+	
 }
--- hsp/src/main/java/com/hsp/user/User.java
+++ hsp/src/main/java/com/hsp/user/User.java
@@ -10,6 +10,7 @@ public class User implements Serializable {
     private String address;
     private String detail_address;
     private String zip_code;
+    private String email;
     private String bank_name;
     private String account_number;
     private String auth;
@@ -21,10 +22,10 @@ public class User implements Serializable {
     public User() {
     	
     }
-    
+
 	public User(String user_id, String phone, String name, String password, String address, String detail_address,
-			String zip_code, String bank_name, String account_number, String auth, String regit_date, int itemStart,
-			int itemSizePerPage) {
+			String zip_code, String email, String bank_name, String account_number, String auth, String regit_date,
+			int itemStart, int itemSizePerPage) {
 		this.user_id = user_id;
 		this.phone = phone;
 		this.name = name;
@@ -32,6 +33,7 @@ public class User implements Serializable {
 		this.address = address;
 		this.detail_address = detail_address;
 		this.zip_code = zip_code;
+		this.email = email;
 		this.bank_name = bank_name;
 		this.account_number = account_number;
 		this.auth = auth;
@@ -39,81 +41,115 @@ public class User implements Serializable {
 		this.itemStart = itemStart;
 		this.itemSizePerPage = itemSizePerPage;
 	}
+
 	public String getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getDetail_address() {
 		return detail_address;
 	}
+
 	public void setDetail_address(String detail_address) {
 		this.detail_address = detail_address;
 	}
+
 	public String getZip_code() {
 		return zip_code;
 	}
+
 	public void setZip_code(String zip_code) {
 		this.zip_code = zip_code;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getBank_name() {
 		return bank_name;
 	}
+
 	public void setBank_name(String bank_name) {
 		this.bank_name = bank_name;
 	}
+
 	public String getAccount_number() {
 		return account_number;
 	}
+
 	public void setAccount_number(String account_number) {
 		this.account_number = account_number;
 	}
+
 	public String getAuth() {
 		return auth;
 	}
+
 	public void setAuth(String auth) {
 		this.auth = auth;
 	}
+
 	public String getRegit_date() {
 		return regit_date;
 	}
+
 	public void setRegit_date(String regit_date) {
 		this.regit_date = regit_date;
 	}
+
 	public int getItemStart() {
 		return itemStart;
 	}
+
 	public void setItemStart(int itemStart) {
 		this.itemStart = itemStart;
 	}
+
 	public int getItemSizePerPage() {
 		return itemSizePerPage;
 	}
+
 	public void setItemSizePerPage(int itemSizePerPage) {
 		this.itemSizePerPage = itemSizePerPage;
 	}
@@ -121,11 +157,10 @@ public class User implements Serializable {
 	@Override
 	public String toString() {
 		return "User [user_id=" + user_id + ", phone=" + phone + ", name=" + name + ", password=" + password
-				+ ", address=" + address + ", detail_address=" + detail_address + ", zip_code=" + zip_code
-				+ ", bank_name=" + bank_name + ", account_number=" + account_number + ", auth=" + auth + ", regit_date="
-				+ regit_date + ", itemStart=" + itemStart + ", itemSizePerPage=" + itemSizePerPage + "]";
+				+ ", address=" + address + ", detail_address=" + detail_address + ", zip_code=" + zip_code + ", email="
+				+ email + ", bank_name=" + bank_name + ", account_number=" + account_number + ", auth=" + auth
+				+ ", regit_date=" + regit_date + ", itemStart=" + itemStart + ", itemSizePerPage=" + itemSizePerPage
+				+ "]";
 	}
-	
-	
     
 }
