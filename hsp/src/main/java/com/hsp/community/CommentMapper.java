f98cb508a0e0a751ba79ccfea262518295689470
이학승 <64819737+hoohahj@users.noreply.github.com>
2021-06-03 14:07:05 +0900

    댓글

---
+++ hsp/src/main/java/com/hsp/community/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.hsp.community;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentMapper {
+	public List<Comment> list() throws Exception;
+
+	public void insert(Comment comment) throws Exception;
+
+	public void update(Comment comment) throws Exception;
+
+	public void delete(Comment comment) throws Exception;
+}
